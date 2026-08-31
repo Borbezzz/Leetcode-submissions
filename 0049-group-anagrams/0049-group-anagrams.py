5ea1a38018701265bb2380e9251c996481dab004
Yixin <yixin.gao2017@gmail.com>
2026-08-31 23:23:31 +0200

    Time: 16 ms (27.11%), Space: 23.9 MB (17.16%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,29 +1,12 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        hash = {}
-        temp_arr = []
-        final_arr = []
+        groups = {}
 
         for word in strs:
-            for chrt in word:
-                hash[chrt] = hash.get(chrt, 0) + 1
-            temp_arr.append((word, hash.copy()))
-            hash.clear()
+            count = [0] * 26
+            for c in word:
+                count[ord(c) - ord('a')] += 1
+            key = tuple(count)
+            groups.setdefault(key, []).append(word)
 
-        used = [False] * len(temp_arr)
-
-        for i in range(len(temp_arr)):
-            if used[i]:
-                continue
-            group = [temp_arr[i][0]]
-            used[i] = True
-            for j in range(len(temp_arr)):
-                if i != j and temp_arr[i][1] == temp_arr[j][1]:
-                    group.append(temp_arr[j][0])
-                    used[j] = True
-            final_arr.append(group)
-
-        return final_arr
-
-
-
+        return list(groups.values())
